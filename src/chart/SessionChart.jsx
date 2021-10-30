c1276dc9710b62cfa8cda3d55b8541d7be7f578f
Darren <xueyublue@gmail.com>
2021-10-30 23:30:01 +0800

    commit

--- src/chart/SessionChart.jsx
+++ src/chart/SessionChart.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
 
 //-------------------------------------------------------------
 //* COMPONENT START
 //-------------------------------------------------------------
-function SessionChart() {
+function SessionChart({ labels, data }) {
   return <div>Session Chart</div>;
 }
 
