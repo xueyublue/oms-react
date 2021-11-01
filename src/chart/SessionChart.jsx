43f3c0f2fa53ace0ea35828a9fe2e817748a254f
Darren <xueyublue@gmail.com>
2021-11-01 12:13:12 +0800

    updated session chart page

--- src/chart/SessionChart.jsx
+++ src/chart/SessionChart.jsx
@@ -1,11 +1,121 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
+import { Line } from "react-chartjs-2";
+import { useSnackbar } from "notistack";
+import Loading from "../components/Loading";
+import ApiCallFailed from "../components/ApiCallFailed";
+import { BackendAPIContext } from "../context/BackendAPIContext";
+import { API_FETCH_WAIT } from "../util/constants";
+
+const getMaxValue = (data) => {
+  let max = data[0];
+  for (let d of data) {
+    if (d >= max) max = d;
+  }
+  if (max < 100) max = 100;
+  else if (max < 150) max = 150;
+  else if (max < 200) max = 100;
+  else max = 500;
+  return max;
+};
 
 //-------------------------------------------------------------
 //* COMPONENT START
 //-------------------------------------------------------------
-function SessionChart({ labels, data }) {
-  return <div>Session Chart</div>;
+function SessionChart({ type }) {
+  const [pageLoad, setPageLoad] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState(null);
+  const { baseUrl } = useContext(BackendAPIContext);
+  const { enqueueSnackbar } = useSnackbar();
+
+  const fetchData = async () => {
+    setTimeout(() => {
+      axios
+        .get(`${baseUrl}/sessions/monitoring`)
+        .then(({ data }) => {
+          setData(data);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          setData(null);
+          setIsLoading(false);
+          console.log(err);
+        });
+    }, API_FETCH_WAIT);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetchData();
+    }, API_FETCH_WAIT * 5);
+    return () => clearInterval(interval);
+  }, [baseUrl]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  if (isLoading) return <Loading />;
+  if (!data) {
+    enqueueSnackbar("Failed to connect backend services. Please check the network connection.", {
+      variant: "error",
+      autoHideDuration: 5000,
+    });
+    return <ApiCallFailed />;
+  }
+  //* display snackbar only one time to inform the refresh interval
+  if (!pageLoad) {
+    setPageLoad(true);
+    enqueueSnackbar("Sessions loaded, refresh interval: 5 seconds.", {
+      variant: "success",
+      autoHideDuration: 5000,
+    });
+  }
+
+  let maxValue = 100;
+  if (data) maxValue = getMaxValue(data.total);
+
+  const dataSource = {
+    labels: data.time,
+    datasets: [
+      {
+        label: "Total",
+        data: data.total,
+        fill: true,
+        borderColor: "rgb(36, 209, 209)",
+        tension: 0.3,
+      },
+      {
+        label: "Active",
+        data: data.active,
+        fill: true,
+        borderColor: "rgb(253, 211, 100)",
+        tension: 0.3,
+      },
+      {
+        label: "Inactive",
+        data: data.inactive,
+        fill: true,
+        borderColor: "rgb(75, 122, 192)",
+        tension: 0.3,
+      },
+    ],
+  };
+  const options = {
+    plugins: {
+      title: { display: true, text: "Sessions (every 10s)" },
+      legend: {
+        position: "right",
+      },
+    },
+    stepped: true,
+    animation: {
+      duration: 500,
+    },
+    maintainAspectRatio: false,
+    scales: {
+      y: { beginAtZero: true, max: maxValue },
+    },
+  };
+
+  return <Line data={dataSource} options={options} />;
 }
 
 export default SessionChart;
