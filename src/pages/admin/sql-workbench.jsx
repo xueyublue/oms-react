a93b54c67005e90e97f670218ac9d194bc52c00a
Xue Yu <xueyublue@gmail.com>
2021-11-05 12:53:34 +0800

    updated sqlworbnech

--- src/pages/admin/sql-workbench.jsx
+++ src/pages/admin/sql-workbench.jsx
@@ -1,15 +1,70 @@
-import React from "react";
-import { Result } from "antd";
+import React, { useState } from "react";
+import { Tabs, Table, Input } from "antd";
+import { MenuOutlined, FileSearchOutlined, AudioOutlined } from "@ant-design/icons";
+import { withStyles } from "@mui/styles";
+
+const { TabPane } = Tabs;
+const { Search } = Input;
+
+//-------------------------------------------------------------
+//* STYLES START
+//-------------------------------------------------------------
+const styles = {
+  root: {
+    width: "100%",
+  },
+};
 
 //-------------------------------------------------------------
 // PAGE START
 //-------------------------------------------------------------
-const SQLWorkbench = () => {
+const SQLWorkbench = ({ classes }) => {
+  const [activeKey, setActiveKey] = useState(1);
+  const handleChange = () => {};
+  const handleEdit = () => {};
+
   return (
-    <div>
-      <Result status="warning" title="The requested page is yet to be developed." />
+    <div className={classes.root}>
+      <Tabs type="editable-card">
+        <TabPane
+          tab={
+            <span>
+              <MenuOutlined />
+              Explorer
+            </span>
+          }
+          key={1}
+        >
+          <Table style={{ marginTop: "-15px" }} />
+        </TabPane>
+        <TabPane
+          tab={
+            <span>
+              <MenuOutlined />
+              Explorer
+            </span>
+          }
+          key={2}
+        >
+          <Table style={{ marginTop: "-15px" }} />
+        </TabPane>
+        <TabPane
+          tab={
+            <span>
+              <FileSearchOutlined />
+              SQL
+            </span>
+          }
+          key={3}
+        >
+          <div style={{ width: "100%", marginTop: "-10px" }}>
+            <Search placeholder="Intelligent SQL Inputter" allowClear enterButton size="middle" />
+            <Table style={{ marginTop: "10px" }} />
+          </div>
+        </TabPane>
+      </Tabs>
     </div>
   );
 };
 
-export default SQLWorkbench;
+export default withStyles(styles)(SQLWorkbench);
