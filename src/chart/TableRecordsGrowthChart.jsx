72186f9e74635f5ca4d69abc8884c629ff6d3f1f
Darren <xueyublue@gmail.com>
2021-12-10 10:05:31 +0800

    updated

--- src/chart/TableRecordsGrowthChart.jsx
+++ src/chart/TableRecordsGrowthChart.jsx
@@ -9,18 +9,30 @@ import ChartDataLabels from "chartjs-plugin-datalabels";
 function TableRecordsGrowthChart({ displayTitle, data, displayLimit, onDisplayLimitChange, displayData = true }) {
   const [form] = Form.useForm();
   const [type, setType] = useState("linear");
+  const [growthType, setGrowthType] = useState("Daily");
 
   if (!data) return;
   let labels = [];
   let records = [];
   let bgColors = [];
+
   for (let i = 0; i < data.length; i++) {
-    if (data[i].dailyGrowth < 1000) bgColors[i] = "rgba(36, 209, 209, 0.8)";
-    else bgColors[i] = "rgba(253, 211, 153, 0.8)";
+    if (growthType === "Daily")
+      if (data[i].dailyGrowth < 1000) bgColors[i] = "rgba(36, 209, 209, 0.8)";
+      else bgColors[i] = "rgba(253, 211, 153, 0.8)";
+    else if (growthType === "Monthly")
+      if (data[i].monthlyGrowth < 1000 * 30) bgColors[i] = "rgba(36, 209, 209, 0.8)";
+      else bgColors[i] = "rgba(253, 211, 153, 0.8)";
+    else if (growthType === "Yearly")
+      if (data[i].yearlyGrowth < 1000 * 365) bgColors[i] = "rgba(36, 209, 209, 0.8)";
+      else bgColors[i] = "rgba(253, 211, 153, 0.8)";
     labels[i] = data[i].owner + "." + data[i].tableName;
-    records[i] = data[i].dailyGrowth;
+    if (growthType === "Daily") records[i] = data[i].dailyGrowth;
+    else if (growthType === "Monthly") records[i] = data[i].monthlyGrowth;
+    else if (growthType === "Yearly") records[i] = data[i].yearlyGrowth;
     if (i > displayLimit) break;
   }
+
   const dataSource = {
     labels: labels,
     datasets: [
@@ -52,7 +64,12 @@ function TableRecordsGrowthChart({ displayTitle, data, displayLimit, onDisplayLi
         position: "top",
         beginAtZero: true,
         min: 0,
-        max: data[0].currentCount,
+        max:
+          growthType === "Daily"
+            ? data[0].dailyGrowth
+            : growthType === "Monthly"
+            ? data[0].monthlyGrowth
+            : data[0].yearlyGrowth,
       },
     },
   };
@@ -82,6 +99,20 @@ function TableRecordsGrowthChart({ displayTitle, data, displayLimit, onDisplayLi
           >
             {["Linear", "Logarithmic"].map((value) => (
               <Select.Option value={value.toLowerCase()} key={value}>
+                {value}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item label="Growth Type" style={{ width: 200 }}>
+          <Select
+            value={growthType}
+            onChange={(value) => {
+              setGrowthType(value);
+            }}
+          >
+            {["Daily", "Monthly", "Yearly"].map((value) => (
+              <Select.Option value={value} key={value}>
                 {value}
               </Select.Option>
             ))}
