ab3e12f82cded9dd2892e006bd9dd6198e51d6dc
Xue Yu <xueyublue@gmail.com>
2022-01-18 10:36:51 +0800

    renamed

--- src/context/BackendAPIContext.jsx
+++ src/context/BackendAPIContext.jsx
@@ -5,7 +5,7 @@ export const BackendAPIContext = createContext();
 export const BackendAPIProvider = (props) => {
   const [baseUrl, setBaseUrl] = useState("");
   const hostname = window.location.hostname;
-  const newBaseUrl = `http://${hostname}:8090/wms7/rest/oms`;
+  const newBaseUrl = `http://10.33.1.166:8090/wms7/rest/oms`;
   if (newBaseUrl !== baseUrl) setBaseUrl(newBaseUrl);
   return <BackendAPIContext.Provider value={{ baseUrl }}>{props.children}</BackendAPIContext.Provider>;
 };
